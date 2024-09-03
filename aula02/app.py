c288a4270e483da8b90b5c27d2a8e82eeba431ff
Brendow Cavalcante Nascimento <brendow.c@yahoo.com>
2024-09-03 15:55:11 -0300

    adding the class 2

---
+++ aula02/app.py
@@ -0,0 +1,18 @@
+
+nome = "Brendow"
+idade = "26"
+gosto_de = "Programação"
+
+print(f"meu nome é {nome}.")
+print(f"tenho {idade} anos de idade.")
+print(f"E gosto muito de {gosto_de}!")
+
+nome = "lucas" # alterando o valor da variável
+print(f"meu nome é {nome}.")
+
+# tipos de dados
+meu_nome = "leandro" #string
+minha_idade = 24 # int 
+minha_altura = 1.70 #float
+eh_homem = True
+eh_casado = False
