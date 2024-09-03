b7c381945e5cfc4942de6e9fccdaa4038fcc3668
Brendow Cavalcante Nascimento <brendow.c@yahoo.com>
2024-09-03 17:08:46 -0300

    adding class 3

---
+++ aula03/app.py
@@ -0,0 +1,25 @@
+
+num1 = 10
+num2 = 10.5
+
+# type retorna o tipo de dado
+print(type(num1))
+print(type(num2))
+
+# converter um int para float
+print(float(15))
+
+# operadores aritmeticos
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+print(10 % 10) # resto da divisao
+print(10 ** 2) # exponenciacao
+
+
+# funcoes
+print("funcoes               - ")
+print(pow(5,2)) #exponenciacao
+print(max(10,5,15,20)) # retorna o max entre esses numeros
+print(min(10,5,15,20)) # retorna o min entre esses numeros
